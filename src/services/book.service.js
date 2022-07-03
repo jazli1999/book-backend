fea09f38505ec9c96d3995e38d31a1dfe63f7b31
ge32max <ge32max@mytum.de>
2022-07-03 20:23:54 +0200

    Fetch Single Book from JSON

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -1,6 +1,6 @@
 // const mongoose = require('mongoose');
 // const Book = require('../models/book.model');
-import request from 'superagent';
+import request from 'superagent'
 
 async function modifyInput(json) {
     const books = { search_result: [] };
@@ -60,19 +60,45 @@ async function modifyInput(json) {
 // fix size of 20 or 50
 async function searchGbooks(query) {
     // google books api
-    let booksJson;   
-    const r = await request
-        .get('https://www.googleapis.com/books/v1/volumes')
-        .query({ q: query, maxResults: 30 }) // For now I set max results to 30
+    var books_json;
+    var input_json = JSON.parse(query)
+
+
+    // Rename Parameters
+    input_json.intitle = input_json.title
+    input_json.inauthor = input_json.author
+    input_json.inpublisher = input_json.publisher
+
+    // Delete useless parameters
+    delete input_json.title
+    delete input_json.author
+    delete input_json.publisher
+    Object.keys(input_json).forEach(key => input_json[key] === undefined || input_json[key] === null && delete input_json[key])
+
+
+    // Create temp query string in googlebooks format
+    var temp_query = [];
+    for (var p in input_json)
+        if (input_json.hasOwnProperty(p)) {
+            temp_query.push(encodeURIComponent(p) + ":" + encodeURIComponent(input_json[p]));
+        }
+    temp_query = temp_query.join("+")
+
+    // Send request to google books api with implemented query string
+    var r = await request
+        .get("https://www.googleapis.com/books/v1/volumes")
+        .query( { q : temp_query, maxResults: 30} ) // For now I set max results to 30
         .then((data) => {
             // return the results
-            booksJson = JSON.parse(data.text);
-            booksJson = booksJson.items;
-        });
+            books_json = JSON.parse(data.text)
+            books_json = books_json.items
+
+        })
     // modifies the input json so that we can access our model
     // parameters much easierPromise.resolve('Success').then( modifyInput(books_json))
     // I couldn't solve async issue without using await back to back      
-    return await modifyInput(booksJson);
+    return modifyInput(books_json)
 }
+
 
 export default { searchGbooks };
