92333337eafbcdbd3fad1f3d854af15000d79ff8
Yanjie L <jasli1999@outlook.com>
2022-07-07 21:05:56 +0200

    add get orders for a user

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,44 +1,54 @@
-import OrderService from "../services/order.service.js";
+import OrderService from '../services/order.service.js';
+
+const getUserOrders = (req, res) => {
+    OrderService.readUserOrders(req.userId).then((orders) => {
+        res.status(200).json(orders);
+    });
+};
 
 const getOrder = (req, res) => {
-  OrderService.read(req.params.id).then((order) => {
-    if (order) {
-      const response = JSON.parse(JSON.stringify(order))
-      response.reqId = req.userId;
-      res.status(200).json(response);
-    } else {
-      res.status(404).send('Resource not found');
-    }
-  });
-}
+    OrderService.read(req.params.id).then((order) => {
+        if (order) {
+            const response = JSON.parse(JSON.stringify(order));
+            response.reqId = req.userId;
+            res.status(200).json(response);
+        }
+        else {
+            res.status(404).send('Resource not found');
+        }
+    });
+};
 
 async function updatePayment(req, res) {
-  const { isReq, payment } = req.body;
-  OrderService.updatePayment(req.params.id, isReq, req.userId, payment).then((status) => {
-    res.setHeader('content-type', 'text/plain');
-    res.status(status).send('ok');
-  });
+    const { isReq, payment } = req.body;
+    OrderService.updatePayment(req.params.id, isReq, req.userId, payment)
+        .then((status) => {
+            res.setHeader('content-type', 'text/plain');
+            res.status(status).send('ok');
+        });
 }
 
 async function updateTrackingCode(req, res) {
-  const { isReq, trackingCode } = req.body;
-  OrderService.updateTrackingCode(req.params.id, isReq, req.userId, trackingCode).then((status) => {
-    res.setHeader('content-type', 'text/plain');
-    res.status(status).send('ok');
-  });
+    const { isReq, trackingCode } = req.body;
+    OrderService.updateTrackingCode(req.params.id, isReq, req.userId, trackingCode)
+        .then((status) => {
+            res.setHeader('content-type', 'text/plain');
+            res.status(status).send('ok');
+        });
 }
 
 async function confirmReceipt(req, res) {
-  const { isReq } = req.body;
-  OrderService.confirmReceipt(req.params.id, isReq, req.userId).then((status) => {
-    res.setHeader('content-type', 'text/plain');
-    res.status(status).send('ok');
-  });
+    const { isReq } = req.body;
+    OrderService.confirmReceipt(req.params.id, isReq, req.userId).then((status) => {
+        res.setHeader('content-type', 'text/plain');
+        res.status(status).send('ok');
+    });
 }
 
 export default {
-  getOrder,
-  updatePayment,
-  updateTrackingCode,
-  confirmReceipt,
-}
+    getUserOrders,
+    getOrder,
+    updatePayment,
+    updateTrackingCode,
+    confirmReceipt,
+};
