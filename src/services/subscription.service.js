c20f0fa1a0cd14c749ed30ef66ce3a168dd058b8
ge32max <ge32max@mytum.de>
2022-07-16 20:15:14 +0200

    New Get Module For Subscription, Get Now Checks if still premium

--- src/services/subscription.service.js
+++ src/services/subscription.service.js
@@ -9,15 +9,21 @@ async function create(userId, subsModel) {
         user.premimum = {};
         const start = new Date();
         const end = new Date(start);
-        if( subsModel != 'undefined'){
-            if(subsModel === 'monthly'){
-                end.setMonth(end.getMonth() + 1)
+        if( subsModel !== undefined){
+            if( subsModel !== 'free'){
+                if(subsModel === 'monthly'){
+                    end.setMonth(end.getMonth() + 1)
+                }
+                else if(subsModel === 'yearly'){
+                    end.setFullYear(end.getFullYear() + 1)
+                }
+                user.premium = { "isPremium": true, "startDate" : start, "endDate" : end };
             }
-            else if(subsModel === 'yearly'){
-                end.setFullYear(end.getFullYear() + 1)
+            else {
+                user.premium = { "isPremium": false, "startDate" : start, "endDate" : end };
             }
         }
-        user.premium = { "isPremium": true, "startDate" : start, "endDate" : end };
+        
     }
     user.save();
     return user.premium;
@@ -30,7 +36,7 @@ async function update(userId, subsModel) {
         if (user === null) return 'no such user';
         if (user.premium !== undefined || !user.premimum.isPremium ) {
             user.premium.endDate = new Date();
-            if( subsModel != 'undefined'){
+            if( subsModel != undefined){
                 if(subsModel === 'monthly'){
                     user.premium.endDate.setMonth(user.premium.endDate.getMonth() + 1)
                 }
@@ -48,24 +54,43 @@ async function update(userId, subsModel) {
 }
 
 
-async function get(userId) {
+async function getDetails(userId) {
     const user = await User.findById(userId);
     if (user === null) return 'no such user';
     if (user.premium !== undefined || !user.premimum.isPremium) {
+        today = new Date();
+        if (today >  user.premium.stopDate){
+            await cancel(userId);
+        }
         return user.premium;
     }
     return {"Status" : "Free Member"}
+}
+
+async function get(userID) {
+    const user = await User.findById(userId);
+    if (user === null) return 'no such user';
+    if (user.premium !== undefined) {
+        today = new Date();
+        if (today >  user.premium.stopDate){
+            await cancel(userID);
+        }
+    }
+    else{
+        create(userID, 'free');
+    }
+    return user.premium.isPremium;
 }
 
 async function cancel(userId) {
     const user = await User.findById(userId);
     user.premium.isPremium = false;
     user.premium.startDate = null;
-    user.premium.startDate = null;
+    user.premium.stopDate = null;
     user.save();
     return 'subscription cancelled';
 }
 
 export default {
-    create, update, get, cancel
+    create, update, get, getDetails, cancel
 };
